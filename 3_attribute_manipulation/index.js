edc15cfda5f86e8e8022e3ae2bc4e40629d63daa
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-08-12 03:27:30 +0600

    set attribute, remove attribute

---
+++ 3_attribute_manipulation/index.js
@@ -0,0 +1,9 @@
+/* var myattr = document.querySelector('a').getAttribute("href");
+
+console.log(myattr); */
+
+
+// $('a').attr('href');
+// $('a').removeAttr('href');
+
+$('a').attr('href', 'https://www.khshakil.com');
