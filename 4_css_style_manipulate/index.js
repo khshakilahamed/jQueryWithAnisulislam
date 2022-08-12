94273cf53ce8736279e0222b3303340108f13112
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-08-12 10:31:59 +0600

    css style manipulation

---
+++ 4_css_style_manipulate/index.js
@@ -0,0 +1,9 @@
+/* $('h1').css('color', 'red');
+$('h1').css('font-size', '3rem');
+$('h1').css('font-style', 'italic');
+
+
+$('h1').css({'color': 'green', 'font-size': '2rem'});
+ */
+
+$("h1").addClass("style1 style2");
