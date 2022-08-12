b0a3749f7309f48e1f20c1b8451a3730ca40d66f
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-08-12 11:20:16 +0600

    hide show, fadeIn fadeOut with toggle, slideUp slideDown wih toggle

---
+++ 7_animation_part_1/index.js
@@ -0,0 +1,26 @@
+$('#showHide').click(function(){
+    // $('#div1').hide(1000);
+    // $('#div1').hide(1000).show(1000);
+
+    $('#div1').toggle(1000);
+});
+
+$('#fadeInFadeOut').click(function(){
+    // $('#div1').fadeOut(1000);
+    // $('#div1').fadeOut(2000).fadeIn(2000);
+    $('#div1').fadeToggle(2000);
+});
+
+$('#fadeTo').click(function(){
+    /* -----------------------
+    $('#div1').fadeTo(time, opacity);
+    --------------------------*/
+    $('#div1').fadeTo(2000, 0.4);
+});
+
+$('#slideUpSlideDown').click(function(){
+    // $('#div1').slideUp(2000);
+    // $('#div1').slideDown(2000);
+    // $('#div1').slideUp(2000).slideDown(2000);
+    $('#div1').slideToggle(2000);
+});
